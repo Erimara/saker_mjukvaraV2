c5d2346ff60466e0136c1b0c07e1c731f51eff14
Erimara <erimarandersson@gmail.com>
2024-01-23 21:46:37 +0100

    added unqie email registration

--- server/src/http_methods/user_methods.rs
+++ server/src/http_methods/user_methods.rs
@@ -1,5 +1,6 @@
 use actix_web::{HttpResponse, web};
 use bcrypt::{DEFAULT_COST, hash};
+use mongodb::bson::doc;
 use mongodb::Database;
 use crate::models::user::User;
 use crate::utils::validate::email_validation;
@@ -21,6 +22,14 @@ pub(crate) async fn register(data: web::Data<Database>, user: web::Json<User>) -
             return HttpResponse::InternalServerError().finish();
         }
     };
+
+    let does_email_exist = doc! { "email": &is_valid_email };
+    if let Ok(Some(_email_exists)) = collection.find_one(does_email_exist.clone(), None).await {
+        return HttpResponse::InternalServerError().finish();
+    } else if let Err(e) = collection.find_one(does_email_exist, None).await {
+        return HttpResponse::InternalServerError().finish();
+    }
+
     let hashed_user = User{
         id: None,
         email:is_valid_email,
--- server/src/utils/validate.rs
+++ server/src/utils/validate.rs
@@ -2,6 +2,7 @@ use validator::{validate_email};
 
 pub fn email_validation(email: String) -> Result<String, &'static str> {
     if validate_email(&email) {
+
         return Ok(email)
     } else {
         Err("Invalid email")
